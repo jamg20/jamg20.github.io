bebcd5629a2f74623ec9c96cca9790018ad2d1ee
jamg20 <alberto_murillo1995@outlook.com>
2025-03-27 14:37:53 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,17 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login</title>
-   	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="styles/paginas/index.css">
-    <link rel="icon" type="image/png" href="imagenes/icobeto.png">
 </head>
 <body>
-    <div class="spinner-container" id="loadingSpinner">
-        <div class="spinner-border text-light" role="status">
-            <span class="visually-hidden">Cargando...</span>
-        </div>
-    </div>
-
     <div class="formulario">
         <div class="text-center">
             <img src="imagenes/beto.png" class="logo" alt="Logo">
@@ -35,22 +26,6 @@
             <div class="d-grid">
                 <button type="submit" class="btn btn-danger">Aceptar</button>
             </div>
-
-        	<!--PARA REGISTRAR USUARIO EN EL LOGIN ESTARA OCULTO
-			<!--<div class="mt-3 text-center">
-                <a href="Registro_Usuario.php">¡Regístrate Aquí!</a>
-            </div>-->
-        </form>
     </div>
-
-    <script>
-        function mostrarSpinner() {
-            document.getElementById('loadingSpinner').style.display = 'flex';
-        }
-    </script>
-	
-
-    <!-- Enlace a Bootstrap JS y dependencias -->
-    <script src="js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
